f0714e9f5b950538c1aabae041af48a0702f5bff
Brian Mara <brian@blendit.eu>
2024-06-08 15:35:06 +0800

    Create component files and templates

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Income</h4>
+      <p id="money-plus" class="money plus">+$0.00</p>
+    </div>
+    <div>
+      <h1>Expense</h1>
+      <p id="money-minus" class="money minus">-$0.00</p>
+    </div>
+  </div>
+</template>
